dcd21948af9c692d6127d13795bcc60b862474bc
Oluwatobi <oluwatobioreniyi@gmail.com>
2025-03-13 17:26:34 +0100

    Add Header component and Button UI, update package dependencies

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,32 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
   return (
-    <header>
-        <section>
-            <div>
-                <Image src={'https://i.postimg.cc/gcVsypZn/Whatbytes-logo-2.png'} alt='' width={300} height={300}/>
-            </div>
-            <h1>WhatBytes</h1>
-        </section>
+    <header className="flex justify-between items-center h-28 w-full p-5 border">
+      <div className="flex items-center">
+        <Image
+          src={"https://i.postimg.cc/gcVsypZn/Whatbytes-logo-2.png"}
+          alt=""
+          width={300}
+          height={300}
+          className="size-16"
+        />
+        <h1 className="text-4xl font-bold">WhatBytes</h1>
+      </div>
+        <Button variant={"outline"}>
+          <Image
+            src={"https://i.postimg.cc/gcVsypZn/Whatbytes-logo-2.png"}
+            alt=""
+            width={300}
+            height={300}
+            className="size-6 rounded-full bg-purple-400"
+          />
+          <p>Rahill Siddique</p>
+        </Button>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
